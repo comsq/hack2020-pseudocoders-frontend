79d0c0a1e28335f680d17193d45a3c2a84306e13
ubogov <s.ubogov@corp.mail.ru>
2020-10-30 19:28:25 +0300

    добавил стили для ant

--- src/components/AppHeader/AppHeader.tsx
+++ src/components/AppHeader/AppHeader.tsx
@@ -1,11 +1,13 @@
 import { Link } from '@reach/router';
 import React from 'react';
 import styles from 'src/components/AppHeader/AppHeader.module.css';
+import { Button } from 'antd';
 
 export function AppHeader() {
     const id = 1;
     return (
         <div className={styles.appHeader}>
+            <Button type="primary">Button</Button>
             <Link to="/courses">Курсы</Link>
             <Link to={`/users/${id}`}>Профиль</Link>
         </div>
