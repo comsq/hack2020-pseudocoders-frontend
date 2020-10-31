ae135f11760d32aeee036eb13c98b6e87e763d73
Irina Sedova <irihka.sedova2011@yandex.ru>
2020-10-31 15:35:15 +0500

    send create task

--- src/stores/Create.ts
+++ src/stores/Create.ts
@@ -1,35 +1,61 @@
-import { WithLoadingFlags } from 'src/helpers/StoreHelper';
-import { AsyncHelper } from 'src/helpers/AsyncHelper';
+import { makeAutoObservable } from 'mobx';
+
+export interface Language {
+    name: string;
+    slug: string;
+    id: string;
+}
+
+const HOST_API = 'http://api.pseudocoders.online/api/';
+// const commonBody = {
+//     mode: 'cors',
+//     headers: {
+//         'Access-Control-Allow-Origin':'*'
+//     }
+// };
 
 function getApi() {
     return {
-        async loadList() {
-            await AsyncHelper.delay(1000);
-            return [
-                {
-                    name: 'Курс по PHP',
-                    description: 'php ето не сила',
-                },
-                {
-                    name: 'Курс по JS',
-                    description: 'js ето сила',
-                },
-                {
-                    name: 'Курс по C',
-                    description: 'C ето немножко сила',
-                },
-            ];
+        async loadLanguages() {
+            const response = await fetch(`${HOST_API}languages/`, { method: 'get' });
+            if (response.ok) {
+                return response.json();
+            }
+        },
+
+        async saveTask(data: any) {
+            console.log(JSON.stringify(data));
+            const response = await fetch(`${HOST_API}tasks/`, { method: 'post', body: JSON.stringify(data) });
+            return response.status;
         },
     };
 }
 
-type Create = {
-    name: string;
-    description: string;
-};
 class CreateStoreClass {
     api = getApi();
-    list = new WithLoadingFlags<Create[]>(this.api.loadList);
+    languages = [];
+    saveProcess = false;
+    saveStatus = 0;
+
+    constructor() {
+        makeAutoObservable(this);
+    }
+
+    setLanguages(languages: any) {
+        this.languages = languages;
+    }
+
+    async getLanguages() {
+        const lang = await this.api.loadLanguages();
+        this.setLanguages(lang);
+    }
+
+    async saveTask(data: any) {
+        this.saveProcess = true;
+        const status = await this.api.saveTask(data);
+        this.saveProcess = false;
+        this.saveStatus = status;
+    }
 }
 
 export const CreateStore = new CreateStoreClass();
